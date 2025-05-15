dea6556a0207ab3cc4ec37f95b14375e550f9229
FrankieNC <101644758+FrankieNC@users.noreply.github.com>
2025-05-15 17:08:31 +0000

    Deploying to gh-pages from @ FrankieNC/FrankieNC.github.io@c20ee69a03e66c36f38dbd2b958d4d681dd359ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,6 +518,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/fchotuck", "_blank");
         },
       },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
